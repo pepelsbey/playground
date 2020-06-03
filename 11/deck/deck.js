d5fb402dcdf879d9e9e37ff4a7809096931560ca
Vadim Makeev <pepelsbey@gmail.com>
2020-06-04 00:07:14 +0300

    Правит табы на пробелы и добавляет конфиг

--- 11/deck/deck.js
+++ 11/deck/deck.js
@@ -1,19 +1,19 @@
 customElements.define('shower-deck', class extends HTMLElement {
-	constructor() {
-		super();
-		
-		this.attachShadow({
-			mode: 'open'
-		});
-		
-		const template = document.createElement('template');
-		template.innerHTML = `
-			<link rel="stylesheet" href="deck.css">
-			<slot></slot>
-		`;
-		
-		this.shadowRoot.appendChild(
-			template.content.cloneNode(true)
-		);
-	}
+    constructor() {
+        super();
+
+        this.attachShadow({
+            mode: 'open'
+        });
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <link rel="stylesheet" href="deck.css">
+            <slot></slot>
+        `;
+
+        this.shadowRoot.appendChild(
+            template.content.cloneNode(true)
+        );
+    }
 });
--- 12/video.js
+++ 12/video.js
@@ -1,51 +1,51 @@
 function findVideos() {
-	let videos = document.querySelectorAll('.video');
+    let videos = document.querySelectorAll('.video');
 
-	for (let i = 0; i < videos.length; i++) {
-		setupVideo(videos[i]);
-	}
+    for (let i = 0; i < videos.length; i++) {
+        setupVideo(videos[i]);
+    }
 }
 
 function setupVideo(video) {
-	let link = video.querySelector('.video__link');
-	let media = video.querySelector('.video__media');
-	let button = video.querySelector('.video__button');
-	let id = parseMediaURL(media);
+    let link = video.querySelector('.video__link');
+    let media = video.querySelector('.video__media');
+    let button = video.querySelector('.video__button');
+    let id = parseMediaURL(media);
 
-	video.addEventListener('click', () => {
-		let iframe = createIframe(id);
+    video.addEventListener('click', () => {
+        let iframe = createIframe(id);
 
-		link.remove();
-		button.remove();
-		video.appendChild(iframe);
-	});
+        link.remove();
+        button.remove();
+        video.appendChild(iframe);
+    });
 
-	link.removeAttribute('href');
-	video.classList.add('video--enabled');
+    link.removeAttribute('href');
+    video.classList.add('video--enabled');
 }
 
 function parseMediaURL(media) {
-	let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-	let url = media.src;
-	let match = url.match(regexp);
+    let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+    let url = media.src;
+    let match = url.match(regexp);
 
-	return match[1];
+    return match[1];
 }
 
 function createIframe(id) {
-	let iframe = document.createElement('iframe');
+    let iframe = document.createElement('iframe');
 
-	iframe.setAttribute('allowfullscreen', '');
-	iframe.setAttribute('src', generateURL(id));
-	iframe.classList.add('video__media');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('src', generateURL(id));
+    iframe.classList.add('video__media');
 
-	return iframe;
+    return iframe;
 }
 
 function generateURL(id) {
-	let query = '?rel=0&showinfo=0&autoplay=1';
+    let query = '?rel=0&showinfo=0&autoplay=1';
 
-	return 'https://www.youtube.com/embed/' + id + query;
+    return 'https://www.youtube.com/embed/' + id + query;
 }
 
 findVideos();
